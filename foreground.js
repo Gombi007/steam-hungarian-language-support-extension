1ffea65879d9ef17e62970843f10e0a4f80e5c56
Gyula Gombos <gombos88@gmail.com>
2022-07-25 17:44:43 +0200

    popup html was updated

--- foreground.js
+++ foreground.js
@@ -1,25 +1,34 @@
 if (document.getElementById("magyarositas") === null) {
-  let element = document.createElement("a");
-  let elementTXT = document.createTextNode("Magyarosítás");
-  element.setAttribute("id", "magyarositas");
-  element.setAttribute(
+  let gameTitle = document.getElementById("appHubAppName").innerHTML;
+  gameTitle = gameTitle.replaceAll("™", "");
+
+  //create a container
+  let container = document.createElement("a");
+  container.setAttribute("id", "magyaritas");
+  container.setAttribute(
     "href",
-    "https://magyaritasok.hu/games/search?search=" +
-      document.getElementById("appHubAppName").innerHTML
+    "https://magyaritasok.hu/games/search?search=" + gameTitle
   );
-  element.appendChild(elementTXT);
-  element.classList.add("btnv6_blue_hoverfade");
+  container.classList.add("btnv6_blue_hoverfade");
+
+  //add my own magyarositasok link
+  let element = document.createElement("p");
+  let elementTXT = document.createTextNode("Magyarítás");
+  element.appendChild(elementTXT);  
   element.classList.add("language-support-link");
-  document.getElementsByClassName("apphub_OtherSiteInfo")[0].prepend(element);
+  container.appendChild(element);
+  
+  //add check icon to steam
+  let iconCheck = document.createElement("i");
+  iconCheck.classList.add("gg-search");
+  container.appendChild(iconCheck);  
+  
+  document
+  .getElementsByClassName("apphub_OtherSiteInfo")[0]
+  .prepend(container);
 }
 
-//add check icon to steam
-/*
-let iconCheck = document.createElement("i")
-iconCheck.classList.add("gg-check")
-document.getElementsByClassName("language-support-link")[0].appendChild(iconCheck)
-*/
-
+// just test for API
 function communicatingWithMagyarositasokHU() {
   fetch("https://magyaritasok.hu/games/search?search=cyberpunk-2077", {
     headers: { "Access-Control-Allow-Origin": "*" },
@@ -30,11 +39,3 @@ function communicatingWithMagyarositasokHU() {
       console.error(error);
     });
 }
-/*
-let contentDiv = document.createElement("div");
-contentDiv.setAttribute('id','content-00');
-contentDiv.classList.add("btnv6_blue_hoverfade");
-contentDiv.classList.add("content");
-document.getElementsByClassName("apphub_OtherSiteInfo")[0].prepend(contentDiv);
-*/
-//add my own element to steam page
