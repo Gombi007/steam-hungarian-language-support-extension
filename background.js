d4e309f6d7d4a61a0fbd6a76a2bb7366691d6f78
Gyula Gombos <gombos88@gmail.com>
2022-07-25 18:30:05 +0200

    bugfix searchbox showed twice

--- background.js
+++ background.js
@@ -1,18 +1,19 @@
-chrome.tabs.onUpdated.addListener(
-  (tabId, changeInfo, tab) => {
-       if (/^https:\/\/store\.steampowered\.com\/app/.test(changeInfo.url)) {
-      chrome.scripting.insertCSS({
-        target: { tabId: tabId },
-        files: ['./all.css']})
-      
-        chrome.scripting.insertCSS({
-          target: { tabId: tabId },
-          files: ['./language-support.css']})
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (/^https:\/\/store\.steampowered\.com\/app\/.*\/$/.test(changeInfo.url) && changeInfo.status == "loading" ) {
+    console.log(changeInfo);
+    chrome.scripting.insertCSS({
+      target: { tabId: tabId },
+      files: ["./all.css"],
+    });
 
-      chrome.scripting.executeScript({
-        target: { tabId: tabId, allFrames: false },
-        files: ["./foreground.js"],
-      });
-    }
-  });
+    chrome.scripting.insertCSS({
+      target: { tabId: tabId },
+      files: ["./language-support.css"],
+    });
 
+    chrome.scripting.executeScript({
+      target: { tabId: tabId, allFrames: false },
+      files: ["./foreground.js"],
+    });
+  }
+});
