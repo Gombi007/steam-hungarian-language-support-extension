a216701fef657ededf55479d9fc8d6b45b6bb24a
Gyula Gombos <gombos88@gmail.com>
2022-07-25 11:45:46 +0200

    add style to my link

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
     if (/^https:\/\/store\.steampowered\.com\/app/.test(current_tab_info.url)) {
+      chrome.scripting.insertCSS({
+        target: { tabId: tab.tabId },
+        files: ['./language-support.css']})
+
       chrome.scripting.executeScript({
         target: { tabId: tab.tabId, allFrames: false },
         files: ["./foreground.js"],
--- foreground.js
+++ foreground.js
@@ -1,1 +1,9 @@
-console.log("Hello from the foreground");
+let element = document.createElement("a");
+let elementTXT = document.createTextNode("Magyarosítás");
+element.setAttribute('href', "http://google.com");
+element.appendChild(elementTXT);
+element.classList.add('btnv6_blue_hoverfade');
+element.classList.add('language-support-link');
+
+console.log(document.getElementsByClassName('apphub_OtherSiteInfo')[0].appendChild(element));
+console.log(document.getElementsByClassName('apphub_OtherSiteInfo')[0]);
